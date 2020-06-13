559d37448397030c28001b238b0aae838ea0a7dc
Eknoor Oberoi <eknoor.oberoi@gmail.com>
2020-06-13 23:32:04 +0530

    Applied Level-1 authentication,register users with Username and Password

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req,res){
   res.render("home");
@@ -26,6 +33,36 @@ app.get("/register", function(req,res){
   res.render("register");
 });
 
+app.post("/register", function(req,res){
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+//user can only view secrets page, if he has successfully been created on database
+  newUser.save(function(err){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login", function(req,res){
+  const username= req.body.username;
+  const password= req.body.password;
+  User.findOne({email: username}, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        if(foundUser.password === password){
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
